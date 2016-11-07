8dd307b5be68ec519342c523610881140fc1097c
polmon <polsoft@polmon.com>
2016-11-07 15:15:14 +0530

    changes

--- filereader/src/test/java/com/wordcount/filereader/UserFileReaderServicesTest.java
+++ filereader/src/test/java/com/wordcount/filereader/UserFileReaderServicesTest.java
@@ -1,11 +1,17 @@
 package com.wordcount.filereader;
 
+import java.util.List;
+import java.util.Map;
+
+import com.filereader.IUserFileReader;
+import com.filereader.UserFileReader;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple UserFileReaderServicesTest
  */
 public class UserFileReaderServicesTest 
     extends TestCase
@@ -15,9 +21,22 @@ public class UserFileReaderServicesTest
      *
      * @param testName name of the test case
      */
-    public UserFileReaderServicesTest( String testName )
+    public UserFileReaderServicesTest( String  filePath)
     {
-        super( testName );
+    	String filePath1 = "/home/polmon/Documents/workspace-sts-3.8.2.RELEASE/filereader/src/main/java/com/wordcount/filereader/Soujanya";
+    	IUserFileReader userFileReader = new UserFileReader();
+    	
+    	Map<Integer, String> readMap = userFileReader.readFileAsMap(filePath1);
+		List<String> readList = userFileReader.readFileAsList(filePath1);
+    	
+		for (Integer lineNumber : readMap.keySet()) {
+			System.out.println(lineNumber+". "+readMap.get(lineNumber));
+		}
+		
+		for (String lineData : readList) {
+			System.out.println(lineData);
+		}
+        super(filePath1);
     }
 
     /**
